bdb2b5545af1ce80eea1fe2e14ff2b10398e63ac
The-Freaky-Fox <The-Freaky-Fox@users.noreply.github.com>
2015-01-30 19:53:21 +0000

    Added logging to ModBlocks. Fixes #1

--- src/main/java/thefreakyfox/advancedmod/init/ModBlocks.java
+++ src/main/java/thefreakyfox/advancedmod/init/ModBlocks.java
@@ -1,11 +1,14 @@
 package thefreakyfox.advancedmod.init;
 
 import thefreakyfox.advancedmod.reference.Reference;
+import thefreakyfox.advancedmod.util.LogHelper;
 import cpw.mods.fml.common.registry.GameRegistry.ObjectHolder;
 
 @ObjectHolder( Reference.MOD_ID )
 public class ModBlocks {
 
-	public static void init() {}
+	public static void init() {
+		LogHelper.info( "ModBlocks called" );
+	}
 
 }
