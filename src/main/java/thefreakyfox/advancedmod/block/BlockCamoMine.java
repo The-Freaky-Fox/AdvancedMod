527c8b239e67d8ddc2ba74afbfbe16f6b12f7b19
The-Freaky-Fox <The-Freaky-Fox@users.noreply.github.com>
2015-02-02 00:01:48 +0000

    The Camo Mine can now have its camouflage set on a per-side basis

--- src/main/java/thefreakyfox/advancedmod/block/BlockCamoMine.java
+++ src/main/java/thefreakyfox/advancedmod/block/BlockCamoMine.java
@@ -1,6 +1,7 @@
 package thefreakyfox.advancedmod.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
@@ -35,7 +36,19 @@ public class BlockCamoMine extends BlockAdvancedModTileEntity {
 			final TileEntity te = world.getTileEntity( x, y, z );
 			if ( te instanceof TileEntityCamoMine && te != null ) {
 				final TileEntityCamoMine teMine = ( TileEntityCamoMine ) te;
-				teMine.setCamoStack( player.getCurrentEquippedItem() );
+
+				if ( teMine.getCamoStack( side ) != null ) {
+					final ItemStack camoStack = teMine.getCamoStack( side );
+					teMine.setCamoStack( null, side );
+					final EntityItem itemEntity = new EntityItem( world, x, y, z, camoStack );
+					world.spawnEntityInWorld( itemEntity );
+				} else {
+					final ItemStack playerItem = player.getCurrentEquippedItem();
+					if ( playerItem != null ) {
+						final ItemStack camoStack = playerItem.splitStack( 1 );
+						teMine.setCamoStack( camoStack, side );
+					}
+				}
 			}
 		}
 		return true;
@@ -47,7 +60,7 @@ public class BlockCamoMine extends BlockAdvancedModTileEntity {
 		final TileEntity te = world.getTileEntity( x, y, z );
 		if ( te instanceof TileEntityCamoMine && te != null ) {
 			final TileEntityCamoMine teMine = ( TileEntityCamoMine ) te;
-			final ItemStack stack = teMine.getCamoStack();
+			final ItemStack stack = teMine.getCamoStack( side );
 			if ( stack != null && stack.getItem() instanceof ItemBlock ) {
 				final Block block = ( ( ItemBlock ) stack.getItem() ).field_150939_a;
 				return block.getIcon( side, stack.getItemDamage() );
--- src/main/java/thefreakyfox/advancedmod/tileentity/TileEntityCamoMine.java
+++ src/main/java/thefreakyfox/advancedmod/tileentity/TileEntityCamoMine.java
@@ -2,20 +2,25 @@ package thefreakyfox.advancedmod.tileentity;
 
 import io.netty.buffer.ByteBuf;
 
+import java.util.Arrays;
 import java.util.List;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.AxisAlignedBB;
+import thefreakyfox.advancedmod.init.ModBlocks;
 import cpw.mods.fml.common.network.ByteBufUtils;
 
 
-public class TileEntityCamoMine extends TileEntityAdvancedMod {
+public class TileEntityCamoMine extends TileEntityAdvancedMod implements ISidedInventory {
 
 	private int timer = 60;
-	private ItemStack camoStack;
+	private final ItemStack[] camoStacks = new ItemStack[6];
 
 	@Override
 	public void updateEntity() {
@@ -30,23 +35,24 @@ public class TileEntityCamoMine extends TileEntityAdvancedMod {
 		}
 	}
 
-	public ItemStack getCamoStack() {
-		return camoStack;
+	public ItemStack getCamoStack( int side ) {
+		return getStackInSlot( side );
 	}
 
-	public void setCamoStack( ItemStack stack ) {
-		camoStack = stack;
-		worldObj.markBlockForUpdate( xCoord, yCoord, zCoord );
+	public void setCamoStack( ItemStack stack, int side ) {
+		setInventorySlotContents( side, stack );
 	}
 
 	@Override
 	public void writeToPacket( ByteBuf buf ) {
-		ByteBufUtils.writeItemStack( buf, camoStack );
+		for ( final ItemStack stack : camoStacks )
+			ByteBufUtils.writeItemStack( buf, stack );
 	}
 
 	@Override
 	public void readFromPacket( ByteBuf buf ) {
-		camoStack = ByteBufUtils.readItemStack( buf );
+		for ( int i = 0; i < camoStacks.length; i++ )
+			camoStacks[i] = ByteBufUtils.readItemStack( buf );
 		worldObj.markBlockRangeForRenderUpdate( xCoord, yCoord, zCoord, xCoord, yCoord, zCoord );
 	}
 
@@ -55,11 +61,18 @@ public class TileEntityCamoMine extends TileEntityAdvancedMod {
 		super.writeToNBT( tag );
 		tag.setInteger( "timer", timer );
 
-		if ( camoStack != null ) {
-			final NBTTagCompound t = new NBTTagCompound();
-			camoStack.writeToNBT( t );
-			tag.setTag( "camoStack", t );
+		final NBTTagList camoStackTag = new NBTTagList();
+
+		for ( int i = 0; i < camoStacks.length; i++ ) {
+			final ItemStack stack = camoStacks[i];
+			if ( stack != null ) {
+				final NBTTagCompound t = new NBTTagCompound();
+				stack.writeToNBT( t );
+				t.setByte( "index", ( byte ) i );
+				camoStackTag.appendTag( t );
+			}
 		}
+		tag.setTag( "camoStacks", camoStackTag );
 	}
 
 	@Override
@@ -67,11 +80,158 @@ public class TileEntityCamoMine extends TileEntityAdvancedMod {
 		super.readFromNBT( tag );
 		timer = tag.getInteger( "timer" );
 
-		if ( tag.hasKey( "camoStack" ) ) {
-			camoStack = ItemStack.loadItemStackFromNBT( tag.getCompoundTag( "camoStack" ) );
-		} else {
-			camoStack = null;
+		Arrays.fill( camoStacks, null );
+		final NBTTagList camoStackTag = tag.getTagList( "camoStacks", 10 );
+
+		for ( int i = 0; i < camoStackTag.tagCount(); i++ ) {
+			final NBTTagCompound t = camoStackTag.getCompoundTagAt( i );
+			final int index = t.getByte( "index" );
+			if ( index >= 0 && index < camoStacks.length )
+				camoStacks[index] = ItemStack.loadItemStackFromNBT( t );
 		}
 	}
+
+
+	/**
+	 * Returns the number of slots in the inventory.
+	 */
+	@Override
+	public int getSizeInventory() {
+		return camoStacks.length;
+	}
+
+	/**
+	 * Returns the stack in slot i
+	 */
+	@Override
+	public ItemStack getStackInSlot( int slot ) {
+		return camoStacks[slot];
+	}
+
+	/**
+	 * Removes from an inventory slot (first arg) up to a specified number (second arg) of items and
+	 * returns them in a
+	 * new stack.
+	 */
+	@Override
+	public ItemStack decrStackSize( int slot, int amount ) {
+		if ( camoStacks[slot] != null ) {
+			ItemStack itemstack;
+
+			if ( camoStacks[slot].stackSize <= amount ) {
+				itemstack = camoStacks[slot];
+				setInventorySlotContents( slot, null );
+				markDirty();
+				return itemstack;
+			} else {
+				itemstack = camoStacks[slot].splitStack( amount );
+
+				if ( camoStacks[slot].stackSize == 0 ) {
+					setInventorySlotContents( slot, null );
+				}
+
+				markDirty();
+				return itemstack;
+			}
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * When some containers are closed they call this on each slot, then drop whatever it returns as
+	 * an EntityItem -
+	 * like when you close a workbench GUI.
+	 */
+	@Override
+	public ItemStack getStackInSlotOnClosing( int slot ) {
+		if ( camoStacks[slot] != null ) {
+			final ItemStack itemstack = camoStacks[slot];
+			camoStacks[slot] = null;
+			return itemstack;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Sets the given item stack to the specified slot in the inventory (can be crafting or armor
+	 * sections).
+	 */
+	@Override
+	public void setInventorySlotContents( int slot, ItemStack stack ) {
+		camoStacks[slot] = stack;
+
+		if ( stack != null && stack.stackSize > getInventoryStackLimit() ) {
+			stack.stackSize = getInventoryStackLimit();
+		}
+
+		markDirty();
+		worldObj.markBlockForUpdate( xCoord, yCoord, zCoord );
+	}
+
+	/**
+	 * Returns the name of the inventory
+	 */
+	@Override
+	public String getInventoryName() {
+		return ModBlocks.camoMine.getUnlocalizedName() + ".name";
+	}
+
+	/**
+	 * Returns if the inventory is named
+	 */
+	@Override
+	public boolean hasCustomInventoryName() {
+		return false;
+	}
+
+	/**
+	 * Returns the maximum stack size for a inventory slot.
+	 */
+	@Override
+	public int getInventoryStackLimit() {
+		return 1;
+	}
+
+	/**
+	 * Do not make give this method the name canInteractWith because it clashes with Container
+	 */
+	@Override
+	public boolean isUseableByPlayer( EntityPlayer player ) {
+		return worldObj.getTileEntity( xCoord, yCoord, zCoord ) != this ? false : player.getDistanceSq( xCoord + 0.5D,
+				yCoord + 0.5D, zCoord + 0.5D ) <= 64.0D;
+	}
+
+	@Override
+	public void openInventory() {}
+
+	@Override
+	public void closeInventory() {}
+
+	/**
+	 * Returns true if automation is allowed to insert the given stack (ignoring stack size) into
+	 * the given slot.
+	 */
+	@Override
+	public boolean isItemValidForSlot( int slot, ItemStack stack ) {
+		return stack != null && stack.getItem() instanceof ItemBlock;
+	}
+
+	@Override
+	public int[] getAccessibleSlotsFromSide( int side ) {
+		return new int[] { side };
+	}
+
+	@Override
+	public boolean canInsertItem( int slot, ItemStack stack, int side ) {
+		return isItemValidForSlot( slot, stack );
+	}
+
+	@Override
+	public boolean canExtractItem( int slot, ItemStack stack, int side ) {
+		return true;
+	}
+
 
 }
