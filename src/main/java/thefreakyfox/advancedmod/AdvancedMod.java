15e9d50a38662771dcad875b85a68b0f3bf9ea74
The-Freaky-Fox <The-Freaky-Fox@users.noreply.github.com>
2015-03-02 16:07:26 +0000

    Actually initialised the DescriptionHandler

--- src/main/java/thefreakyfox/advancedmod/AdvancedMod.java
+++ src/main/java/thefreakyfox/advancedmod/AdvancedMod.java
@@ -2,6 +2,7 @@ package thefreakyfox.advancedmod;
 
 import thefreakyfox.advancedmod.init.ModBlocks;
 import thefreakyfox.advancedmod.init.ModTileEntities;
+import thefreakyfox.advancedmod.network.DescriptionHandler;
 import thefreakyfox.advancedmod.network.NetworkHandler;
 import thefreakyfox.advancedmod.proxy.CommonProxy;
 import thefreakyfox.advancedmod.reference.Reference;
@@ -33,6 +34,7 @@ public class AdvancedMod {
 		proxy.preInit();
 		GameRegistry.registerWorldGenerator( new WorldGeneratorFlag(), 0 );
 		NetworkHandler.init();
+		DescriptionHandler.init();
 		NetworkRegistry.INSTANCE.registerGuiHandler( instance, new GuiHandler() );
 		LogHelper.info( "Pre-initialisation complete!" );
 	}
